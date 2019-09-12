afdb7154c35124ad2f78eea235e6a8d882de5dc2
njugunanduati <njugunanduati@gmail.com>
2019-09-12 14:43:05 +0300

    removed the media root variable

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
